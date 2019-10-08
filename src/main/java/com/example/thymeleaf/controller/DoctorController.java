656c2d1002dddae1b0dd34ddbb3ef86e464ed226
Elodie Tran <mcetran@gmail.com>
2019-10-08 20:33:16 +0200

    Challenge Spring 03 done

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -1,13 +1,17 @@
 package com.example.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DoctorController {
 
     @GetMapping("/doctor")
-    public String doctor() {
+    public String doctor(Model doctorModel, @RequestParam(defaultValue = "John Smith") String name, @RequestParam(defaultValue = "0") String number) {
+        doctorModel.addAttribute("doctorname", name);
+        doctorModel.addAttribute("doctornumber", number);
         return "doctor";
     }
 }
